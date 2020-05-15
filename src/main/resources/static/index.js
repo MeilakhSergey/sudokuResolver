a9b5c08af4dc32c45d5226a63be72c398c5482b4
MeilakhSergey <meylakhsergey@gmail.com>
2020-05-15 13:26:09 +0300

    Adding url to Heroku app

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,5 @@
+const url = "https://sudoku-resolver.herokuapp.com";
+
 function getSudoku() {
     var sudoku = "rows=";
     var item = 0;
@@ -23,7 +25,7 @@ function getNextStep() {
         type: "GET",
         contentType : "application/json",
         data: sudoku,
-        url: "http://localhost:8080/board/next-step",
+        url: url + "/board/next-step",
         success: function(data) {
             if (data !== "") {
                 $("#nextStepResult").text(`Number ${data.digit} at position (${data.i+1};${data.j+1})`);
@@ -43,7 +45,7 @@ function isValid() {
         type: "GET",
         contentType : "application/json",
         data: sudoku,
-        url: "http://localhost:8080/board/valid",
+        url: url + "/board/valid",
         success: function(data) {
             $("#isValidResult").text(data.toString());
         },
